fa2bcb3bce0a5dfd48234c3417eaa9415cde5001
Resse Christophe <christophe.resse@gmail.com>
2017-10-30 18:58:34 +0100

    Force creation of java & resource folders.

---
+++ thot-hibernate/src/main/java/org/heliosphere/thot/hibernate/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides artifacts for Hibernate experimentations.
+ */
+package org.heliosphere.thot.hibernate;
---
+++ thot-hibernate/src/test/java/org/heliosphere/thot/hibernate/test/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides tests on artifacts related to Hibernate experimentations.
+ */
+package org.heliosphere.thot.hibernate.test;
